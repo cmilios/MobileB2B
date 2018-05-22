8a4342e1dfdfe1fbb37bbf2dcf54840e7591c7dd
Chris Milios <dai17117@uom.edu.gr>
2018-05-22 11:18:41 +0300

    login end main menu start

--- app/src/main/java/gr/logistic_i/logistic_i/LoginActivity.java
+++ app/src/main/java/gr/logistic_i/logistic_i/LoginActivity.java
@@ -1,14 +1,42 @@
 package gr.logistic_i.logistic_i;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    String name = new String();
+    String pass = new String();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText n = (EditText)findViewById(R.id.username);
+                EditText p = (EditText)findViewById(R.id.password);
+                name = n.getText().toString();
+                pass = n.getText().toString();
+
+                Creds c = new Creds(name,pass);
+
+                Intent i = new Intent(LoginActivity.this, MainMenu.class);
+                startActivity(i);
+
+
+
+
+            }
+        });
     }
 
 
---
+++ app/src/main/java/gr/logistic_i/logistic_i/MainMenu.java
@@ -0,0 +1,29 @@
+package gr.logistic_i.logistic_i;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainMenu extends AppCompatActivity {
+
+    private ArrayList<String> itemnames = new ArrayList<>();
+    private ArrayList<String> itemq = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.recycler_view_home_screen);
+
+        recyclerView.setHasFixedSize(true);
+        MainMenuAdapter adapter = new MainMenuAdapter( itemnames, itemq,this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+    }
+}
