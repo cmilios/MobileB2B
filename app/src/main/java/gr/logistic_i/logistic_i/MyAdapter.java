941fbd41941a9025226b2932b1016c2390e0c983
Michael Georgiadis <mikegeor@outlook.com.gr>
2018-06-28 14:13:58 +0300

    Bullshit

---
+++ app/src/main/java/gr/logistic_i/logistic_i/MyAdapter.java
@@ -0,0 +1,19 @@
+package gr.logistic_i.logistic_i;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+
+class MyViewHolder extends RecyclerView.ViewHolder{
+    public MyViewHolder(View itemView){
+        super(itemView);
+    }
+
+}
+
+
+public class MyAdapter {
+
+
+
+}
